6d077d418c29b94f84ca12dfb0552049367fd21a
mohamedNeddam <neddam.neddam@gmail.com>
2022-10-12 17:13:01 +0100

    let the recycleView use the adapter

--- app/src/main/java/com/example/quotes/MainActivity.kt
+++ app/src/main/java/com/example/quotes/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.quotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quotes.adapter.ItemAdapter
 import com.example.quotes.data.DataSource
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textview : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        textview = findViewById(R.id.textview)
-//        textview.text = DataSource().loadQuotes().size.toString()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, DataSource().loadQuotes())
+        recyclerView.setHasFixedSize(true)
+
     }
 }
